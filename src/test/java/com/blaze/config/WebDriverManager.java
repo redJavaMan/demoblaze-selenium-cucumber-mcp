92a2f2c8cb4e909143aae391d2cdecc24ad59d24
Mohammed Lukmanudhin <lukmanudhinm@gmail.com>
2025-12-28 00:47:00 +0530

    refactor: Add implicit wait

--- src/test/java/com/blaze/config/WebDriverManager.java
+++ src/test/java/com/blaze/config/WebDriverManager.java
@@ -2,6 +2,7 @@ package com.blaze.config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
 
 public class WebDriverManager {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
@@ -10,6 +11,7 @@ public class WebDriverManager {
         if (driver.get() == null) {
             driver.set(new ChromeDriver());
             driver.get().manage().window().maximize();
+            driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
         return driver.get();
     }
